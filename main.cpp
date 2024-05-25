376a003fcdc6024afca5a8e6709224ae768efdf0
Kirill Belyaev <kirill10121415@gmail.com>
2024-05-25 14:49:36 +0300

    alls ready

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "list"
 #include "ctime"
+#include <fstream>
+#include "string"
+
 using namespace std;
 using namespace chrono;
 using time_mngr = time_point<steady_clock, duration<__int64_t, ratio<1, 1000000000>>>;
@@ -23,6 +26,7 @@ void showMenu(){
     cout << "2. Вывести дерево"; nextLine();
     cout << "3. Операции над деревом"; nextLine();
     cout << "4. Скорость проверки на сбалансированность"; nextLine();
+    cout << "5. Варианты"; nextLine();
 
 }
 
@@ -65,7 +69,7 @@ void showTrunkStream(trunkStream *p, int &count, ofstream& f2) {
     if (p == nullptr) return;
     showTrunkStream(p->prev, count, f2);
     count++;
-    cout << p->str;
+    f2 << p->str;
 }
 
 // AVL-tree -------------
@@ -142,15 +146,6 @@ public:
         print(tmp, false, cur->left);
     }
 
-
-    void destroy(Node *root){
-        if( root )
-        {
-            destroy(root->left);
-            destroy(root->right);
-            delete root;
-        }
-    }
 
 
     int getHeight(Node *cur){
@@ -256,18 +251,6 @@ public:
     }
 
 
-//    bool isBalanced( Node* node ){
-//        if ( !node ){
-//            return true;
-//        }
-//        if ( abs(node->right - node->left) >= 2 ){
-//            return false;
-//        }
-//        return isBalanced(node->left) && isBalanced(node->right);
-//
-//    }
-
-
     void printFile(trunkStream* prev, bool isRight, Node *cur, ofstream& f2) {
         if ( !cur )
             return;
@@ -286,87 +269,11 @@ public:
         }
         int count = 0;//
         showTrunkStream(tmp, count, f2);
-        cout << cur->data << "(" << cur->height << ")" << endl;
+        f2 << cur->data << "(" << cur->height << ")" << endl;
         if (prev)
             prev->str = prev_str;
         tmp->str = "   |";
         printFile(tmp, false, cur->left, f2);
-    }
-
-
-    Node *printInsert(int value, Node *node){
-
-        if ( !node ){
-            return new Node(value);
-        }
-        else if ( value > node->data ){
-            node->right = insert(value, node->right);
-        }else{
-            node->left = insert(value, node->left);
-        }
-        return balance(node);
-    }
-
-
-    int printGetHeight(Node *cur){
-        if (!cur){
-            return 0;
-        }
-        cur->height = max(getHeight(cur->left), getHeight(cur->right)) + 1;
-        return cur->height;
-    }
-
-
-
-    Node *printDeleteNode(int value, Node *node){
-        if ( !node ) return nullptr;
-        if ( value > node->data ) node->right =  deleteNode(value, node->right);
-        else if ( value < node->data) node->left = deleteNode(value, node->left);
-        else{
-            if ( !node->right || !node->left ) {
-
-                if (!node->left && !node->right) {
-                    delete node;
-                    return nullptr;
-                } else if (!node->left) {
-                    Node *tmp = node->right;
-                    delete node;
-                    return balance(tmp);
-                } else if (!node->right) {
-                    Node *tmp = node->left;
-                    delete node;
-                    return balance(tmp);
-                }
-            }else {
-                Node *tmp = findMax(node->left);
-                node->data = tmp->data;
-                node->left = deleteNode(node->data, node->left);
-                return balance(node);
-            }
-        }
-        if( node ){
-            getHeight(node);
-        }
-        return balance(node);
-    }
-
-
-
-    Node *printBalance(Node *cur){
-        getHeight(cur);
-        if ( getHeight(cur->right) - getHeight(cur->left ) == 2 ){
-            if ( getHeight(cur->right->left) > getHeight(cur->right->right) ){
-                cur->right = rightRotate(cur->right);
-            }
-            return leftRotate(cur);
-        }else if ( getHeight(cur->right) - getHeight(cur->left) == -2 ){
-            if ( getHeight(cur->left->left) < getHeight(cur->left->right) ){
-                cur->left = leftRotate(cur->left);
-            }
-            return rightRotate(cur);
-        }
-
-        return cur;
     }
 
 };
@@ -381,6 +288,8 @@ int main() {
     list<int> numbers;
     Tree *tree = new Tree;
     bool balance;
+    int variants;
+    string problem;
 
     srand(time(NULL));
 
@@ -457,28 +366,95 @@ int main() {
                         cout << "Поиск прошел за " << (end - start).count() << "нс";
                         nextLine();
                         break;
+                }
 
+            case 4:
+                start = steady_clock::now();
+                balance = tree->isBalanced(tree->root);
+                end = steady_clock::now();
+                cout << "Дерево " << (balance ? "не" : "") << " сбалансированно";
+                nextLine();
+                cout << "Проверка прошла за " << (end - start).count() / 1000 << " мс";
+                nextLine();
+                break;
 
-                    case 4:
-                        start = steady_clock::now();
-                        balance = tree->isBalanced(tree->root);
-                        end = steady_clock::now();
-                        cout << "Дерево " << (balance ? "не" : "") << " сбалансированно";
-                        nextLine();
-                        cout << "Проверка прошла за " << (end - start).count() / 1000 << " мс";
-                        nextLine();
-                        break;
+            case 5:
+                cout << "Кол-во вариантов: "; cin >> variants; clearStream();
+                vector<int> data;
+                int insertItem = randint();
 
-                    case 5:
+                ofstream variantOut;
+                ofstream keyOut;
+                ofstream answerOut;
 
-                        break;
+                variantOut.open("/Users/kirillbelaev/CLionProjects/AVL-tree/variant.txt");
+                keyOut.open("/Users/kirillbelaev/CLionProjects/AVL-tree/key.txt");
+                answerOut.open("/Users/kirillbelaev/CLionProjects/AVL-tree/answer.txt");
+                for ( int variant = 1; variant <= variants; variant++ ) {
+                    Tree *printTree = new Tree;
+                    data.clear();
 
+                    problem = "ВАРИАНТ " + to_string(variant) + "\n";
+                    answerOut << "ВАРИАНТ " << to_string(variant) << ":\n\n";
+                    keyOut << "ВАРИАНТ " << to_string(variant) << ":\n";
 
-                    default:
-                        cout << "error";
-                        nextLine();
+                    for (int j = 0; j < 10; j++) data.push_back(randint());
+
+                    problem += "1. Создать АВЛ-дерево из элементов: ";
+                    answerOut << "1. Создать АВЛ-дерево из элементов: ";
+                    keyOut << "1. Создать АВЛ-дерево из элементов: ";
+
+                    problem += to_string(data[0]);
+                    answerOut << to_string(data[0]);
+                    keyOut << to_string(data[0]);
+                    for (int j = 1; j < 10; j++){
+                        problem += (", " + to_string(data[j]));
+                        answerOut << ", " + to_string(data[j]);
+                        keyOut << ", " + to_string(data[j]);
+                    }
+                    problem += "\n";
+                    answerOut << "\n\n";
+                    keyOut << "\n";
+                    for (int j = 0; j < 10; j++){
+                        printTree->root = printTree->insert(data[j], printTree->root);
+                        printTree->printFile(nullptr, true, printTree->root, answerOut);
+                        answerOut << "\n-------------------------------------------------------\n\n";
+                    }
+
+                    problem += "2. Вставить элемент " + to_string(insertItem) + " в дерево\n";
+                    answerOut << "2. Вставить элемент " + to_string(insertItem) + " в дерево\n\n";
+                    keyOut << "2. Вставить элемент " + to_string(insertItem) + " в дерево\n";
+
+                    printTree->root = printTree->insert(insertItem, printTree->root);
+                    printTree->printFile(nullptr, true, printTree->root, answerOut);
+                    answerOut << "\n-------------------------------------------------------\n\n";
+
+                    int deleteElem = rand() % 10;
+                    problem += "3. Удалить элемент " + to_string(data[deleteElem]) + " из дерева\n\n";
+                    answerOut << "3. Удалить элемент " + to_string(data[deleteElem]) + " из дерева\n\n";
+                    keyOut << "3. Удалить элемент " + to_string(data[deleteElem]) + " из дерева\n\n";
+
+                    printTree->root = printTree->deleteNode(data[deleteElem], printTree->root);
+                    printTree->printFile(nullptr, true, printTree->root, answerOut);
+                    answerOut << "\n\n";
+                    keyOut << "\n";
+                    variantOut << problem;
+                    printTree->printFile(nullptr, true, printTree->root, keyOut);
 
                 }
+
+                variantOut.close();
+                answerOut.close();
+                keyOut.close();
+
+                break;
+
+
+//                default:
+//                    cout << "error";
+//                    nextLine();
+
         }
     }
 }
+
